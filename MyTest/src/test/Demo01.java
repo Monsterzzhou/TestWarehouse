4af40cc9e2640842707c15014fde1b0c4cac4e22
mrzhou <75471824+Monsterzzhou@users.noreply.github.com>
2020-12-04 16:45:11 +0800

    Update Demo01.java

--- MyTest/src/test/Demo01.java
+++ MyTest/src/test/Demo01.java
@@ -3,5 +3,7 @@ package test;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("hello");
+        //add by zd
+        System.out.println("2020/12/4");
     }
 }
